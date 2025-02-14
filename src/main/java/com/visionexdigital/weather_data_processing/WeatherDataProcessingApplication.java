ae0b8881050d8cb3419c60eed0ecc56043641998
FayasAhmed-98 <teamaliens.b18it@gmail.com>
2025-02-14 18:12:10 +0530

    initial commit

---
+++ src/main/java/com/visionexdigital/weather_data_processing/WeatherDataProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.visionexdigital.weather_data_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDataProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visionexdigital/weather_data_processing/WeatherDataProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visionexdigital.weather_data_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
